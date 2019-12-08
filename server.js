fa224f29b48f362281a61e8b3353de8e7a80807b
dhchan <daniel.hoyin.chan@gmail.com>
2019-12-07 19:24:21 -0800

    heroku deployment

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -49,9 +50,17 @@ app.get('/movie', function handleGetPokemon(req, res) {
   res.json(response)
 })
 
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
-
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
